249218e877d792b08d82ffe66c558beacd8b2607
Daniel Naylor <git@drnaylor.co.uk>
2018-07-15 17:11:33 +0100

    Don't throw an exception if the bot cannot read the message history from a channel.

--- src/main/java/io/github/nucleuspowered/proton/task/UpdateGuildMessageCache.java
+++ src/main/java/io/github/nucleuspowered/proton/task/UpdateGuildMessageCache.java
@@ -1,9 +1,11 @@
 package io.github.nucleuspowered.proton.task;
 
 import com.github.benmanes.caffeine.cache.Cache;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.utils.PermissionUtil;
 
 public class UpdateGuildMessageCache implements Runnable {
 
@@ -16,10 +18,11 @@ public class UpdateGuildMessageCache implements Runnable {
     }
 
     @Override public void run() {
-        for (TextChannel channel : guild.getTextChannels()) {
+        final Member member = this.guild.getMember(this.guild.getJDA().getSelfUser());
+        guild.getTextChannels().stream().filter(x -> PermissionUtil.checkPermission(x, member, Permission.MESSAGE_HISTORY)).forEach(channel -> {
             for (Message message : channel.getHistory().retrievePast(100).complete()) {
                 cache.put(message.getId(), message);
             }
-        }
+        });
     }
 }
